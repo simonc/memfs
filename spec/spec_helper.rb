352f80949c0a66045ce6af5320c3020465c8417e
Simon Courtois <scourtois_github@cubyx.fr>
2017-01-01 16:26:34 +0100

    Adding a new config for RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,18 +3,36 @@ require 'memfs'
 
 Coveralls.wear!
 
-RSpec.configure do |config|
-  config.before :each do
-    MemFs::FileSystem.instance.clear!
-  end
-
-  shared_examples 'aliased method' do |method, original_method|
-    it "##{original_method}" do
-      expect(subject.method(method)).to eq(subject.method(original_method))
-    end
-  end
-end
-
 def _fs
   MemFs::FileSystem.instance
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.disable_monkey_patching!
+  config.warnings = true
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+  # config.profile_examples = 10
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.before { MemFs::FileSystem.instance.clear! }
+end
+
+RSpec.shared_examples 'aliased method' do |method, original_method|
+  it "##{original_method}" do
+    expect(subject.method(method)).to eq(subject.method(original_method))
+  end
 end
